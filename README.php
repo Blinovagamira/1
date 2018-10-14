9b5c72d1753686a59a8651d4a8045d9470b34a99
Blinovagamira <44070651+Blinovagamira@users.noreply.github.com>
2018-10-14 16:04:00 +0300

    Update README.php

--- README.php
+++ README.php
@@ -1,12 +1,11 @@
 <?php
-
   $name='Ира';
   $age='28';
   $email='blinovagamira@gmail.com';
   $city='Санкт-Петербург';
   $about='Маркетолог'
-
 ?>
+
 <!Doctype html>
 <html lang="ru">
  <head>
@@ -14,14 +13,11 @@
    <title>Обо мне</title>
  </head>
   <body>
-
     <h1> Страница пользователя <?=$name ?> </h1>
-
        <p style="margin-bottom: -10px"> Имя: <?= $name ?> </p>
        <p style="margin-bottom: -10px"> Возраст: <?= $age ?> </p>
        <p style="margin-bottom: -10px"> Почта: <a href="mailto:blinovagmira@gmail.com"> <?= $email ?> </a> </p>
        <p style="margin-bottom: -10px"> Город: <?= $city ?> </p>
        <p> О себе: <?= $about ?> </p>
-
- </body>
+  </body>
 </html>
